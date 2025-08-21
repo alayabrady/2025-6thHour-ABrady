bea1a11ff5d992ddb1b4ac6ba265cb367930d604
alaya brady <ab129512@student.wpsok.org>
2025-08-21 14:10:21 -0500

    this is my homework 2.

---
+++ HW2.py
@@ -0,0 +1,28 @@
+#Name:Alaya Brady
+#Class: 6th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("Hello World")
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+number=8
+hero_string="ironman"
+spiderman=True
+#3. Print all three variables on the same print function (at the same time).
+print(number,hero_string,spiderman)
+#4. Create a variable that asks the user to input an integer.
+matthew=int(input("put number here"))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+apple=number+matthew
+print(apple)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(apple/2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+spiderman=False
+#8. Print the value of the boolean variable.
+print(spiderman)
+#9. Create a variable with a number that contains decimals.
+bob=3.14
+#10. Round the number from #9 up or down using the round function.
+print(round(bob))
