3d01e9efa4d2499aa9b38ceebe387024b48696c3
alaya brady <ab129512@student.wpsok.org>
2025-09-08 14:03:31 -0500

    my changes to HW 5!!!!!

--- HW5.py
+++ HW5.py
@@ -14,7 +14,6 @@ empty_list=[]
 #4. Remove the median number from the first list and add it to the second list.
 median=number_list[4]
 empty_list.append(median)
-empty_list.remove(median)
 #5. Remove the first number from the first list and add it to the second list.
 empty_list.append(number_list.pop(0))
 #6. Print both lists.
@@ -27,4 +26,4 @@ print(apple)
 number_list.append(apple)
 #9. Sort the first list from lowest to highest and print it.
 number_list.sort()
-print(number_list)
+print(number_list)   
