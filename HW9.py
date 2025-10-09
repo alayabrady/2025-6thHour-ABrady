53d733d6d6c1a9db59471c6cead20730dd13c4a7
alaya brady <ab129512@student.wpsok.org>
2025-10-09 13:48:57 -0500

    This is HW 9.

--- HW9.py
+++ HW9.py
@@ -11,19 +11,25 @@ list = [random.randint(1,100),random.randint(1,100),random.randint(1,100)]
 print(list)
 #4. Create an if statement that determines which of the three numbers is the highest and prints the result.
 if list[0]>list[1] and list[0]>list[2] :
-    print("list[0] is greater)")
+    print(f"{list[0]} is greater")
     num = list[0]
 elif list[1]>list[2] and list[1]>list[2] :
-    print("list[1] is greater")
+    print(f"{list[1]} is greater")
     num = list[1]
 elif list[2<list[0]] and list[2]<list[1] :
-    print("list[2] is greater")
+    print(f"{list[2]} is greater")
     num = list[2]
 
 #5. Tie the result (the largest number) from #4 to a variable called "num".
 
 #6. Create a nested if statement that prints if num is divisible by 2, divisible by 3, both, or neither.
 if num % 2 == 0:
-    print("num is divisible by 2")
-elif num % 3 == 0:
-    print("num is divisible by 3")
+    if num % 3 == 0:
+        print("num is divisible by 2 and 3")
+    else:
+        print("num is divisible by 2 but not 3")
+else:
+    if num % 3 == 0:
+        print("num is divisible by 3 but not 2")
+    else :
+        print("num is divisible by neither")
